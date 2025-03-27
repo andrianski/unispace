2e5cb916ae612eaf63f12ba90bbaa54a762df81c
andrianski <36228006+andrianski@users.noreply.github.com>
2025-03-27 15:57:03 +0200

    Update config-dist.php

--- config-dist.php
+++ config-dist.php
@@ -7,9 +7,6 @@ $dbname = 'unispace';
 $dbusername = 'root'; // Замени с твоя MySQL потребител
 $dbpassword = ''; // Замени с парола, ако има
 
-$username = 'root'; // Замени с твоя MySQL потребител
-$password = ''; // Замени с парола, ако има
-
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
