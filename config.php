b88b95f3f7605737b28ccda95077e48f015d481c
andrianski <36228006+andrianski@users.noreply.github.com>
2025-03-27 10:27:00 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $dbname = 'unispace';
 //include '../dbpass.php');
 // сложи в горния файл
 $username = 'root'; // Замени с твоя MySQL потребител
-$password = 'zaxscd12'; // Замени с парола, ако има
+$password = ''; // Замени с парола, ако има
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
