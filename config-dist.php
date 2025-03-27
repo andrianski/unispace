07b684c98fa4c0d43dfa2d83690956d8c6a7b919
andrianski <36228006+andrianski@users.noreply.github.com>
2025-03-27 15:06:34 +0200

    Create config-dist.php

---
+++ config-dist.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$dbname = 'unispace';
+//include '../dbpass.php');
+// сложи в горния файл
+
+$dbusername = 'root'; // Замени с твоя MySQL потребител
+$dbpassword = ''; // Замени с парола, ако има
+
+$username = 'root'; // Замени с твоя MySQL потребител
+$password = ''; // Замени с парола, ако има
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Грешка при връзка с базата: " . $e->getMessage());
+}
+?>
+ 
